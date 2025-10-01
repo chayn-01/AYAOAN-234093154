407ac1b218ad2efad18502f5e14d95c685141a1e
M307user <M307user@lab.uc-bcf.local>
2025-10-01 11:18:48 +0800

    Add initial PHP exercise

---
+++ CIT17_PHPExercise/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercise</title>
+</head>
+<body>
+   <h3>LAB 02</h3>
+<ul>
+    <li><a href="intro.php">Introduce Yourself</a></li>
+    <li><a href="math.php">Simple Math</a></li>
+    <li><a href="rectangle.php">Area and Perimeter of a Rectangle</a></li>
+    <li><a href="temp.php">Temperature Converter</a></li>
+    <li><a href="var.php">Swapping Variables</a></li>
+    <li><a href="salary.php">Salary Calculator</a></li>
+    <li><a href="bmi.php">BMI Calculator</a></li>
+    <li><a href="string.php">String Manipulator</a></li>
+    <li><a href="bank.php">Bank Account Simulation</a></li>
+    <li><a href="gsystem.php">Simple Grading System</a></li>
+    <li><a href="currency.php">Currency Converter</a></li>
+    <li><a href="tcost.php">Travel Cost Estimator</a></li>
+</ul>
+
+
+</body>
+</html>
---
+++ CIT17_PHPExercise/intro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduce Yourself</title>
+</head>
+<body>
+    <?php
+
+    $name="Charisse Ayaoan";
+    $age="20 years old";
+    $color="blue and purple";
+
+    echo "Hi, I am" . $name. "I am" . $age. "My favorite color are" . $color;
+    ?>
+</body>
+</html>
